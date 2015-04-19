1561050150db556249693386a0a91341b294aad5
Fenhl <fenhl@fenhl.net>
2015-04-19 00:01:17 +0000

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+#![cfg_attr(test, deny(missing_docs, warnings))]
+#![forbid(unused_variables)]
+
+/// An owned sequence of Unicode scalar values, equivalent to `String`.
+pub struct UString(Vec<char>);
+
+/// A Unicode string slice, equivalent to `&str`.
+pub struct UStr<'a>(&'a [char]);
